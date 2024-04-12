02fa7cf2536a3918f0f7dccc018a070d8aa611f1
0prawin0 <109507913+0prawin0@users.noreply.github.com>
2024-04-12 06:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ Let me help you create a job description that would suit your requirements.
 # Initialize the session state keys
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "The following is a conversation with an AI assistant helping to create a job description. The assistant collects the following data one by one by interacting with the user by asking questions to create the best job description: company_name,job_title, key_skills, soft_skills, location, desired_experience, preferred_experience, about_the_team ."}
+        {"role": "system", "content": "The following is a conversation with an AI assistant helping to create a job description. The assistant collects the following data one by one by interacting with the user by asking questions to create the best job description: company_name, job_title, key_skills, soft_skills, location, desired_experience, preferred_experience, about_the_team ."}
     ]
 
 if "job_title_asked" not in st.session_state:
@@ -58,7 +58,7 @@ def save_jd_to_bucket(jd_text, bucket_name, job_role, designation, company_name)
 
     # Add job description content (include responses from both the user and the assistant)
     for msg in st.session_state["messages"]:
-        if msg['role'] == 'user' or msg['role'] == 'assistant':
+        if msg['role'] in ['user', 'assistant']:
             line = msg['content']
             if line.strip().endswith(':'):
                 doc.add_heading(line, level=1)
@@ -78,10 +78,9 @@ def save_jd_to_bucket(jd_text, bucket_name, job_role, designation, company_name)
 
     return f"Job description saved to {bucket_name}/{file_name}"
 
-
 # Button to submit the job description
 if st.button('Submit'):
-    jd_text = '\n'.join([msg['content'] for msg in st.session_state["messages"] if msg['role'] == 'assistant'])
+    jd_text = '\n'.join([msg['content'] for msg in st.session_state["messages"] if msg['role'] in ['user', 'assistant']])
     
     # Extract job role, designation, and company name from the job description
     job_role = "Unknown"  # Default value
